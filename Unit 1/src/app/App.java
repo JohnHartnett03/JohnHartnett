dcc1722b095a3c139514257a9e25f7fd8b6c40ea
JohnHartnett03 <818826@mystma.org>
2020-04-08 15:53:15 +0000

    Added top 5 movies

--- Unit 1/src/app/App.java
+++ Unit 1/src/app/App.java
@@ -1,7 +1,11 @@
 package app;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+    public static void main(final String[] args) throws Exception {
+        System.out.println("1) The Rock");
+        System.out.println("2) Anchorman");
+        System.out.println("3) Iron Man");
+        System.out.println("2) Stepbrothers");
+        System.out.println("2) Old School");
     }
 }
