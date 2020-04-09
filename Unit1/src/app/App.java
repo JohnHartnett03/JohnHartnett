25e3a48d0a1528bcc5c073fe21dcc874a87afda1
JohnHartnett03 <818826@mystma.org>
2020-04-09 18:57:10 +0000

    added variable

---
+++ Unit1/src/app/App.java
@@ -0,0 +1,32 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int myFavNum = 22;
+        boolean stuckInside = true;
+        byte myAge = 16;
+        char firstLetterOfName = 0;
+        short theYear = 2020;
+        long bobbyD = 700;
+        float startPie = 3.14f;
+        double longPie = 3.14159265;
+
+        System.out.println("Am I stuck inside?");
+        System.out.println(stuckInside);
+        System.out.println("Whats my favorite number?");
+        System.out.println(myFavNum);
+        System.out.println("How old am I?");
+        System.out.println(myAge);
+        System.out.println("What is the first letter of my first name?");
+        System.out.println(firstLetterOfName);
+        System.out.println("What year is it?");
+        System.out.println(theYear);
+        System.out.println("How many days of highschool?");
+        System.out.println(bobbyD);
+        System.out.println("What are the first 3 digits of pi?");
+        System.out.println(startPie);
+        System.out.println("What are the first 7 digits of pi?");
+        System.out.println(longPie);
+
+    }
+}
