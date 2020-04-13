fd0c1fee1e8aa209480a5dd97dca214d066b0820
JohnHartnett03 <818826@mystma.org>
2020-04-13 15:48:57 +0000

    did mad lib

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,31 +2,31 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int myFavNum = 22;
-        boolean stuckInside = true;
-        byte myAge = 16;
-        char firstLetterOfName = 0;
-        short theYear = 2020;
-        long bobbyD = 700;
-        float startPie = 3.14f;
-        double longPie = 3.14159265;
+        String person1 = "BobbyD";
+        String person2 = "Elon Musk";
+        String adjective1 = "nice";
+        String adjective2 = "funny";
+        String verb1 = "ran";
+        String verb2 = "slept";
+        String verb3 = "ate";
+        String food1 = "pizza";
+        String dayOfWeek = "nice";
+        String place1 = "school";
+        String place2 = "nice";
+        String nationality = "American";
+        String noun1 = "rock";
+        String noun2 = "grapes";
+        String number1 = "3";
+        String shape1 = "stars";
 
-        System.out.println("Am I stuck inside?");
-        System.out.println(stuckInside);
-        System.out.println("Whats my favorite number?");
-        System.out.println(myFavNum);
-        System.out.println("How old am I?");
-        System.out.println(myAge);
-        System.out.println("What is the first letter of my first name?");
-        System.out.println(firstLetterOfName);
-        System.out.println("What year is it?");
-        System.out.println(theYear);
-        System.out.println("How many days of highschool?");
-        System.out.println(bobbyD);
-        System.out.println("What are the first 3 digits of pi?");
-        System.out.println(startPie);
-        System.out.println("What are the first 7 digits of pi?");
-        System.out.println(longPie);
+
+        System.out.println("Pizza was inveted by a" + " " + adjective2 + " " + nationality + "chef named" + " " + person1 + ".");
+        System.out.println("To make a Pizza, you need to take a lump of " + noun1 + ", and make a thin, round " + adjective2 + " " + noun1 + ".");
+        System.out.println("Then you cover it with " + adjective2 + " sauce, " + adjective1 + " cheese, and fresh chopped " + noun2 + ".");
+        System.out.println("Next you have to bake it in a very hot " + noun1 + ".");
+        System.out.println("When it is done, cut it into " + number1 + " " + shape1 + ".");
+        System.out.println("Some kids like " + food1 + " pizza the best, but my favorite is the " + food1 + " pizza.");
+        System.out.println("If I could, I would eat pizza " + number1 + " times a day!");
 
     }
 }
