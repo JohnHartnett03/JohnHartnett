f4b8ed6cec4b3dbea21d8fd91d4624db785ebcd1
JohnHartnett03 <818826@mystma.org>
2020-04-14 15:49:42 +0000

    added 3 while statments

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,31 +2,37 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String person1 = "BobbyD";
-        String person2 = "Elon Musk";
-        String adjective1 = "nice";
-        String adjective2 = "funny";
-        String verb1 = "ran";
-        String verb2 = "slept";
-        String verb3 = "ate";
-        String food1 = "pizza";
-        String dayOfWeek = "nice";
-        String place1 = "school";
-        String place2 = "nice";
-        String nationality = "American";
-        String noun1 = "rock";
-        String noun2 = "grapes";
-        String number1 = "3";
-        String shape1 = "stars";
+        int count = 0;
+        String color = "blue";
+        boolean sixFeetTall = true;
+        while (count <= 10) {
+            System.out.println(count);
+            count = count + 1;
+        }
+        
+        while (color == "blue"){
+            System.out.println("John is Happy");
 
+            color = "red";
+        }
 
-        System.out.println("Pizza was inveted by a" + " " + adjective2 + " " + nationality + "chef named" + " " + person1 + ".");
-        System.out.println("To make a Pizza, you need to take a lump of " + noun1 + ", and make a thin, round " + adjective2 + " " + noun1 + ".");
-        System.out.println("Then you cover it with " + adjective2 + " sauce, " + adjective1 + " cheese, and fresh chopped " + noun2 + ".");
-        System.out.println("Next you have to bake it in a very hot " + noun1 + ".");
-        System.out.println("When it is done, cut it into " + number1 + " " + shape1 + ".");
-        System.out.println("Some kids like " + food1 + " pizza the best, but my favorite is the " + food1 + " pizza.");
-        System.out.println("If I could, I would eat pizza " + number1 + " times a day!");
+        while (sixFeetTall == true){
+            System.out.println("You are really tall!");
 
+            sixFeetTall = false;
+        }
+        
+        if (count > 10) {
+            System.out.println("I can count to 10");
+            
+        }
+
+        if (color == "red"){
+            System.out.println("John is sad");
+        }
+
+        if (sixFeetTall == false){
+            System.out.println("You are really short.");
+        }
     }
 }
