9c6f3e30cad8a579d1a831b5aa2a1354d7e0d046
JohnHartnett03 <818826@mystma.org>
2020-04-15 15:43:37 +0000

    fixed variables

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -8,28 +8,28 @@ public class App {
             System.out.println(" lets keep counting.");
         }
 
-        for (int i = 10; i <= 20; i = i +1){
-            System.out.println("The count is " + i);
+        for (int t = 10; t <= 20; t = t +1){
+            System.out.println("The count is " + t);
             System.out.println(" lets keep counting.");
         }
 
-        for (int i = 10; i <= 100; i = i +1){
-            System.out.println("The count is " + i);
+        for (int y = 10; y <= 100; y = y +1){
+            System.out.println("The count is " + y);
             System.out.println(" lets keep counting.");
         }
 
-        for (int i = 1000; i <= 256000; i = i +1){
-            System.out.println("The count is " + i);
+        for (int u = 1000; u <= 256000; u = u +1){
+            System.out.println("The count is " + u);
             System.out.println(" lets keep counting.");
         }
 
-        for (int i = 11; i <= 12; i = i +1){
-            System.out.println("The count is " + i);
+        for (int s = 11; s <= 12; s = s +1){
+            System.out.println("The count is " + s);
             System.out.println(" lets keep counting.");
         }
 
-        for (int i = 10; i <= 0; i = i -1){
-            System.out.println("The count is " + i);
+        for (int f = 10; f <= 0; f = f -1){
+            System.out.println("The count is " + f);
             System.out.println(" lets keep counting.");
         }
     }
