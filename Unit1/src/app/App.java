ee39ae141d5eac93896d7d44a205ab9e2e29ec07
JohnHartnett03 <818826@mystma.org>
2020-04-16 18:32:58 +0000

    GOT IT

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -20,9 +20,10 @@ public class App {
             System.out.println(t);
         }
 
-        for (int j = 1; j <= 4; j = j + 3){
+        for (int j = 1; j <= 9; j = j + 1){
+            System.out.println(j * j);
+            System.out.println(j + 3);
             System.out.println(j);
-            System.out.println(j = 1);
         }
     }
         
