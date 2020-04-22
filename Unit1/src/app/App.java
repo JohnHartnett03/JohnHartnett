f302823d6ff371dc20f61bee8dd8e96860e253a6
JohnHartnett03 <818826@mystma.org>
2020-04-22 16:12:34 +0000

    completed puzzel

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,7 +2,7 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
+        System.out.println("Start first loop");
         for(int i = 20; i >= 0; i --){
             if(i == 10){
                 for(int v = 0; v <= 6; v ++){
@@ -11,8 +11,15 @@ public class App {
             }
             System.out.println(i);
         }
-
-        for(int r = 4; ;)
+        System.out.println("End First loop");
+        System.out.println("Start second loop");
+        System.out.println("Hey!");
+        for(int r = 1; r <=9; r ++){
+            System.out.println(r * 4);
+            if (r % 3 == 0){
+                System.out.println("Hey!");
+            }
+        }
     }
 
 }
