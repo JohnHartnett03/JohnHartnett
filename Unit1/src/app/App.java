cc7d5b503aefdb8a2315951eacce9db436a21b71
JohnHartnett03 <818826@mystma.org>
2020-04-22 15:35:22 +0000

    break

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,16 +3,16 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        for (int i = 0; i <= 19; i ++){
-            System.out.println(i);
-            if (i == 10){
-                
-                for (int e = 0; e <= 4; e ++){
-                    System.out.println(10);
+        for(int i = 20; i >= 0; i --){
+            if(i == 10){
+                for(int v = 0; v <= 6; v ++){
+                    System.out.println("10");
                 }
             }
+            System.out.println(i);
         }
 
+        for(int r = 4; ;)
     }
 
 }
