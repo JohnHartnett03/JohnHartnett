c960a7b66f429d63b3d2bae0c8a01a1234091f71
JohnHartnett03 <818826@mystma.org>
2020-04-23 15:29:42 +0000

    solved puzzle

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,24 +2,26 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Start first loop");
-        for(int i = 20; i >= 0; i --){
-            if(i == 10){
-                for(int v = 0; v <= 6; v ++){
-                    System.out.println("10");
-                }
+        System.out.println("Double or single digit?");
+        for (int i = 20; i >=1; i --){
+            if (i >= 10){
+                System.out.println(i + " is a double digit");
             }
-            System.out.println(i);
-        }
-        System.out.println("End First loop");
-        System.out.println("Start second loop");
-        System.out.println("Hey!");
-        for(int r = 1; r <=9; r ++){
-            System.out.println(r * 4);
-            if (r % 3 == 0){
-                System.out.println("Hey!");
+            else {
+                System.out.println(i + " is a single digit");
             }
         }
+        System.out.println("Nice!");
+        System.out.println("Fast food");
+        for (int v = 1; v <= 10; v ++){
+            if (v % 2 == 0){
+                System.out.println("Fries");
+           }
+           else{
+               System.out.println("Burgers");
+           }
+        } 
+        System.out.println("Nice!"); 
     }
 
 }
