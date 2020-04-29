c62d4940202d448ef75c376e2727a904946ee851
JohnHartnett03 <818826@mystma.org>
2020-04-29 15:23:50 +0000

    completed puzzle

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -6,18 +6,15 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("What is your first name");
-        String name = input.nextLine();
-        System.out.println("What is your last name?");  
-        String lastName = input.nextLine();
-        System.out.println("What is your favorite color?");
-        String color = input.nextLine();
-        System.out.println("How old are you?");
-        String age = input.nextLine();
-        System.out.println("what grade are you in?");
-        String grade = input.nextLine();
-        
-        System.out.println("Hello " + name + lastName + "! You are " + age + " and in grade " + grade +". Your favorite color is " + color);
+        System.out.println("Please enter the password.");
+        String password = input.nextLine();
+       
+
+        if (password.equals("183726073") ){
+            System.out.println("ACCESS GRANTED");
+        } else {
+            System.out.println("ACCESS DENIED");
+        }
         
         
 
