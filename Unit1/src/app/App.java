001abd3244e669c8b82dfe6b78859ea2cab1e019
JohnHartnett03 <818826@mystma.org>
2020-05-05 14:54:37 +0000

    made quiz

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,21 +1,42 @@
 package app;
 import java.util.Scanner;
 
-
 public class App {
     public static void main(String[] args) throws Exception {
+
         Scanner input = new Scanner(System.in);
 
-        System.out.println("What is the count");
+        System.out.println("What number is after 100?");
+        String password = input.nextLine();
 
-        int number = input.nextInt();
-        
-        for (int i = 1; i <= number; i ++){
-            System.out.println(i);
+
+        if (password.equals("101")){
+            System.out.println("CORRECT");
+        } else {
+            System.out.println("INCORRECT");
         }
-        
-        
 
+
+
+            System.out.println("What is the capitol of Minnesota?");
+        String password2 = input.nextLine();
+
+
+        if (password2.equals("St. Paul")){
+            System.out.println("CORRECT");
+        } else {
+            System.out.println("INCORRECT");
+        
+        }
+
+        System.out.println("What is the date?");
+        String password3 = input.nextLine();
+
+
+        if (password3.equals("5/5/2020")){
+            System.out.println("CORRECT");
+        } else {
+            System.out.println("INCORRECT");
+        }
     }
-
 }
