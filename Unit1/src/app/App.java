30814c16495c532c691f012214ac3ce2d5b61602
JohnHartnett03 <818826@mystma.org>
2020-05-08 14:45:55 +0000

    completed mad lib

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -6,37 +6,47 @@ public class App {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("What number is after 100?");
-        String password = input.nextLine();
+        System.out.println("Name a name");
+        String name = input.nextLine();
 
+        System.out.println("Name an adjective");
+        String adjective = input.nextLine();
 
-        if (password.equals("101")){
-            System.out.println("CORRECT");
-        } else {
-            System.out.println("INCORRECT");
-        }
+        System.out.println("Name another adejctive");
+        String adjective2 = input.nextLine();
 
+        System.out.println("Name a food");
+        String food = input.nextLine();
 
+        System.out.println("Name a place");
+        String place = input.nextLine();
 
-            System.out.println("What is the capitol of Minnesota?");
-        String password2 = input.nextLine();
+        System.out.println("Name a past tense verb");
+        String verb1 = input.nextLine();
 
-
-        if (password2.equals("St. Paul")){
-            System.out.println("CORRECT");
-        } else {
-            System.out.println("INCORRECT");
+        System.out.println("Name a verb");
+        String verb2 = input.nextLine();
         
-        }
+        System.out.println("Name a animal");
+        String animal = input.nextLine();
+        
+        System.out.println("Name a plural noun");
+        String noun = input.nextLine();
 
-        System.out.println("What is the date?");
-        String password3 = input.nextLine();
+        System.out.println("Name a liquid");
+        String liquid = input.nextLine();
 
+        System.out.println("Name person");
+        String person = input.nextLine();
 
-        if (password3.equals("5/5/2020")){
-            System.out.println("CORRECT");
-        } else {
-            System.out.println("INCORRECT");
-        }
+        System.out.println("Hello! my name is " + name + "and I have a story to tell you.");
+        System.out.println("It was a " + adjective + ", cold November Day. I woke up to the " + adjective2 + "smell of " + food + "roasting in the " + place + "downstairs.");
+        System.out.println("Yes I live up stairs from " + place + ". I " + verb1 + "down the stairs to see if I could help " + verb2 + "the food.");
+        System.out.println("My mom said, take these fresh " + noun + "to the table.");
+        System.out.println("So I carried a tray full of " + noun + "and a pitcher full of " + liquid + "to the table.");
+        System.out.println("And sitting at the table was none other than " + person + "!");
+        System.out.println("I got so excited that I dropped the tray! This is the stroy about how I met " + person + ".");
+
+        
     }
 }
