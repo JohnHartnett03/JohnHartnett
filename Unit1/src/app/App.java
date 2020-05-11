5ef4b1df04864bce5f0b6ba06ecc40b1c57b7eb1
JohnHartnett03 <818826@mystma.org>
2020-05-11 15:20:47 +0000

    made calc

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -12,29 +12,26 @@ public class App {
         System.out.println("Enter another number");
         int number2 = input.nextInt();
 
-        System.out.println("Enter a function");
-        String function = input.nextLine();
+        System.out.println("Press 1 for addition");
+        System.out.println("Press 2 for subtraction");
+        System.out.println("Press 3 for multiplication");
+        System.out.println("Press 4 for division");
+        int function = input.nextInt();
 
-        if (function.equals("+")){
+        if (function == 1){
             System.out.println(number1 + number2);
         }
 
-        if (function.equals("*")){
-            System.out.println(number1 * number2);
-        }
-
-        if (function.equals("-")){
+        if (function == 2){
             System.out.println(number1 - number2);
         }
 
-        if (function.equals("/")){
+        if (function == 3){
+            System.out.println(number1 * number2);
+        }
+        if (function == 4){
             System.out.println(number1 / number2);
         }
 
-        
-
-        
-
-        
-    }
+    }   
 }
