5969889ae3a9df23085eebcbfd7643b78e34c301
JohnHartnett03 <818826@mystma.org>
2020-06-04 17:20:11 +0000

    finished project

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
 
+        boolean correct = false;
+
         Scanner input = new Scanner(System.in);
 
 
@@ -20,6 +22,7 @@ public class App {
       System.out.println("How much money do you have?");
 
       int total = input.nextInt();
+      input.nextLine();
 
       System.out.println("Account created");
 
@@ -33,16 +36,50 @@ public class App {
            String password2 = input.nextLine();
                 if (password2.equals(password)){
                     System.out.println("Login succesfull.");
+                    correct = true;
                 }
                 else {
-                    System.out.println("Incorrect.");
+                    System.out.println("Login failed.");
+                    
                 }
         }
         
         else {
-            System.out.println("incorrect");
+            System.out.println("Login failed.");
+        }
+        if (correct == true){
+        System.out.println("You have " + total + " dollars.");
+        System.out.println("Press 1 to deposite money.");
+        System.out.println("Press 2 to withdraw money.");
+        System.out.println("Press 3 to change your password.");
+
+        int function = input.nextInt();
+
+        if (function == 1){
+            System.out.println("How much money would you like to deposite.");
+
+            int deposite = input.nextInt();
+
+            System.out.println("Your new total is " + (total + deposite) + ".");
         }
 
+        if (function == 2){
+            System.out.println("How much money would you like to withdraw.");
+
+            int withdraw = input.nextInt();
+
+            System.out.println("Your new total is " + (total - withdraw) + ".");
+        }
+
+        if (function == 3){
+            System.out.println("What would you like your new password to be?.");
+
+            password = input.nextLine();
+
+            System.out.println("Your new password if " + password + ".");
+        }
+
+    }
        
 
       
